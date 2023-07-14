6b411a8561feb3227b612fc5759a8eef2a019893
EdvardasNavickas <nps.anapus@gmail.com>
2023-07-14 15:05:47 +0300

    Commit'as

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.clear();
-
+console.log("---------------------");
 //1. Palyginti du skaičius a ir b. Išvesti į konsolę, kuris didesnis arba jie lygūs. (4 taškai)
 
 function numberLetter(a, b) {
@@ -15,26 +15,26 @@ function numberLetter(a, b) {
 console.log(numberLetter(12, 4));
 console.log(numberLetter(4, 12));
 console.log(numberLetter(12, 12));
-
+console.log("---------------------");
 //2. Naudojant for ciklą, išvesti į konsolę skaičius nuo 1 iki 10. (5 taškai)
 
 for (let i = 1; i <= 10; i++) {
     console.log(i);
 }
-
+console.log("---------------------");
 //3. Naudojant for ciklą, išvesti į konsolę skaičius nuo 0, 2, 4, 6, 8, 10. (5 taškai)
 
 for (let i = 0; i <= 10; i += 2) {
     console.log(i);
 }
-
+console.log("---------------------");
 //4. Naudojant for ciklą, sugeneruoti penkis atsitiktinius skaičius nuo 1 iki 10. Išvesti juos konsolėje. (5 taškai)
 
 for (let i = 0; i < 5; i++) {
     let numbers = Math.floor(Math.random() * 10) + 1;
     console.log(numbers);
 }
-
+console.log("---------------------");
 //5. Naudojant while ciklą, spausdinti atsitiktinius skaičius nuo 1 iki 10. Paskutinis atspausdintas skaičius turi būti 5. (7 taškai)
 
 let numbersLetters = 0;
@@ -43,7 +43,7 @@ while (numbersLetters !== 5) {
   numbersLetters = Math.floor(Math.random() * 10) + 1;
   console.log(numbersLetters);
 }
-
+console.log("---------------------");
 //6. Sukurti masyvą, kurio ilgis būtų nuo 20 iki 30, o reikšmės būtų skaičiai nuo 10 iki 30. Surasti didžiausią masyvo reikšmę, NENAUDOJANT sort() bei Math.max() funkcijų. (7 taškai)
 
 let ilgis = Math.floor(Math.random() * 11) + 25;
@@ -64,7 +64,7 @@ for (let i = 1; i < array.length; i++) {
 
 console.log('Array:', array);
 console.log('Biggest value:', value);
-
+console.log("---------------------");
 //7. Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 100. Suskaičiuokite kiek yra kiekvienos raidės. (7 taškai)
 
 let arrays = [];
@@ -79,7 +79,7 @@ for (let i = 0; i < 100; i++) {
 
 console.log("Array:", arrays);
 console.log("Similarities:", similar);
-
+console.log("---------------------");
 //8. Parašyti funkciją - lygineSuma. Funkcijos parametrai - du kintamieji. Testų reikalavimai - abu kitamieji turi būti arba skaičiai arba masyvai(negali būti vienas skaičius, kitas masyvas). Jei kintamieji skaičiai, grąžinti skaičių sumą, jei kintamieji masyvai - grąžinti masyvų ilgių sumą. Jei abu kintamieji skaičiai arba masyvai, bet suma nelyginė - grąžinti tekstą, kad suma nelyginė. (10 taškų)
 
 function numeroLatino(a, b) {
@@ -102,8 +102,9 @@ console.log(numeroLatino([12, 15], [10, 67]));
 console.log(numeroLatino([4, 12], [12, 4, 19]));
 console.log(numeroLatino([4, 12], 12, 4));
 console.log(numeroLatino([4, 12], 12, 4, 30));
-
+console.log("---------------------");
 //9. Parašyti funkciją pirminisSkaicius. Funkcija turi vieną kintamąjį. Turi būti patikrinimas, kad kintamasis yra skaičius. Funkcija turi grąžinti ar pateiktas skaičius yra pirminis( pirminis skaičius yra tas, kuris dalinasi tik iš savęs ir tik iš vieneto be liekanos.) (10 taškų)
+console.log("---------------------");
 //10. Parašyti funkciją telefonoNumeris. Funkcija turi priimti vieną kintamąjį - masyvą. Masyvo elementai - skaičiai, ilgis - 10. Funkcija turi grąžinti telefono numerį tokiu formatu - "(XXX) XXX-XXXX". (10 taškų)
 
 function phoneNumber(array) {
